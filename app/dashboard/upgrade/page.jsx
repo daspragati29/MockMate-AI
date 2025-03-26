f079d288f83d658ca26ad6145e5b6431b5c18d1b
Anindya Dolui <105700454+aninxya07@users.noreply.github.com>
2025-03-26 20:26:19 +0530

    Update page.jsx

--- app/dashboard/upgrade/page.jsx
+++ app/dashboard/upgrade/page.jsx
@@ -113,7 +113,7 @@ function Upgrade() {
                   </div>
                   <h2 className="text-3xl font-semibold text-white mb-4">Lifetime Pro Access</h2>
                   <div className="flex items-baseline justify-center gap-2 mb-4">
-                    <span className="text-2xl text-gray-400">$</span>
+                    <span className="text-2xl text-gray-400">₹</span>
                     <span className="text-6xl font-semibold bg-gradient-to-r from-gray-100 to-gray-300 text-transparent bg-clip-text">
                       49
                     </span>
